01f791042cfaf40b6978b27ff820a0cdfcf9b5dc
Joey Passenger <jpasseng@gmail.com>
2020-10-17 09:41:20 -0400

    Updates

---
+++ precache-manifest.b490e84d1df484d2c991c610044627bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f01e9a8c9cd113c8e83f7a6f23da9f9",
+    "url": "/disc-golf/index.html"
+  },
+  {
+    "revision": "713bbbdf1a5a4b4270c3",
+    "url": "/disc-golf/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96d620c50a643ca90bc4",
+    "url": "/disc-golf/static/js/2.af64e12e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/disc-golf/static/js/2.af64e12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713bbbdf1a5a4b4270c3",
+    "url": "/disc-golf/static/js/main.a82e815e.chunk.js"
+  },
+  {
+    "revision": "9e9584b1726918698205",
+    "url": "/disc-golf/static/js/runtime-main.d9b6a405.js"
+  }
+]);
