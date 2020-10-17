c7d4353bde4df4af9c38578e5a5ffc16f2d3546f
Joey Passenger <jpasseng@gmail.com>
2020-10-17 09:12:47 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>Joey!</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = () => <div>Rounds | Courses | Players</div>;
+
+export default Header;
